6642f0bf0ef8b526a3fb8ee25e9db3e447c1d801
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-22 17:21:13 +0330

    header js and style

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ let searchBtn = document.querySelector("#search-btn");
 
 searchBtn.addEventListener("click", () => {
     searchForm.classList.toggle("active");
+    navbar.classList.remove("active");
 })
+
+let navbar = document.querySelector(".navbar");
+let menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+})
+
+window.onscroll = () => {
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+}
